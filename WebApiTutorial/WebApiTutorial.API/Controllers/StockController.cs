58dd68dc0e940a41f4b6b2bb14dc607edd656035
Fırat Alçın <firatalcin04@gmail.com>
2024-07-15 11:35:53 +0300

    Controllers

---
+++ WebApiTutorial/WebApiTutorial.API/Controllers/StockController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiTutorial.API.Data;
+
+namespace WebApiTutorial.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public StockController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var stocks = _context.Stocks.ToList();
+
+            return Ok(stocks);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById([FromRoute] int id) 
+        {
+            var stock = _context.Stocks.Find(id);
+
+            if(stock is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(stock);
+        }
+    }
+}
